270d6cdf7e4d89b022e70d3a29cf30c4d51a0820
Zhengyi Chen <s2018374@sms.ed.ac.uk>
2022-06-17 15:58:38 +0100

    finished day11

--- sols/day11.cpp
+++ sols/day11.cpp
@@ -60,6 +60,51 @@ int getSol1(vector<vector<int>>& initial_state, int step) {
     return (result);
 }
 
+int getSol2(vector<vector<int>>& initial_state) {
+    const int ALL_FLASH_COUNT = 100;
+    int curr_flash_count = 0;
+    int t = 0;
+    while (curr_flash_count != ALL_FLASH_COUNT) {
+        curr_flash_count = 0;
+        ++t;
+
+        set<pair<int, int>> ready_locs;
+        // Insert currently new ready pairs into set
+        for (int r = 0; r < initial_state.size(); r++) {
+            for (int c = 0; c < initial_state[r].size(); c++) {
+                initial_state[r][c] ++;
+                if (initial_state[r][c] > 9) {
+                    ready_locs.insert(pair(r, c));
+                }
+            }
+        }
+        // Expand on frontier until nothing could be expanded on further
+        // This section is mostly referred to from online materials
+        while (ready_locs.size() > 0) {
+            auto this_ready = ready_locs.begin();
+            ready_locs.erase(this_ready);
+            int i = this_ready->first;
+            int j = this_ready->second;
+            initial_state[i][j] = 0;
+            curr_flash_count ++;
+            for (int r = i - 1; r <= i + 1; r++) {
+                for (int c = j - 1; c <= j + 1; c++) {
+                    if (r >= 0 && r < initial_state.size() && 
+                        c >= 0 && c < initial_state[r].size()) {
+                        if (initial_state[r][c] > 0 && initial_state[r][c] < 10) { // not flashed yet
+                            initial_state[r][c] ++;
+                        }
+                        if (initial_state[r][c] > 9) {
+                            ready_locs.insert(pair(r, c));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return t; // if curr_flash_count == 100, exit out loop and return t
+}
+
 void parser(const string& filepath, vector<vector<int>>& empty_state) {
     ifstream inputf (filepath);
     string nextline;
@@ -97,6 +142,7 @@ void test() {
 
     assert(getSol1(test_0, 2) == 9);
     assert(getSol1(test_1, 10) == 204);
+    assert(getSol2(test_1) == (195 - 10));
 }
 
 int main() {
@@ -106,6 +152,11 @@ int main() {
 
     // 1.
     cout << "Part 1: " << getSol1(states, 100) << endl;
+    
+    // 2.
+    states = {};
+    parser("../resource/q11/input", states);
+    cout << "Part 2: " << getSol2(states) << endl;
     return 0;
 }
 
