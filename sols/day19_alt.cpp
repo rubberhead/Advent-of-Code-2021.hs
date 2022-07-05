89a5cf2c9951974667f60cea11d0a43f6283b3ba
rubberhead <s2018374@ed.ac.uk>
2022-07-05 20:52:15 +0100

    Working on day19 (2)

--- sols/day19_alt.cpp
+++ sols/day19_alt.cpp
@@ -1,6 +1,6 @@
 #include <array>
 #include <vector>
-#include <unordered_set>
+#include <set>
 #include <memory>
 #include <algorithm>
 #include <cassert>
@@ -90,7 +90,7 @@ const vector<Matrix_3d> ROTATION_MATRICES_AROUND_Z = {
     }
 };
 
-unique_ptr<vector<Matrix_3d>> filpXHeading (const vector<Matrix_3d>& matrices) {
+unique_ptr<vector<Matrix_3d>> flipXHeading (const vector<Matrix_3d>& matrices) {
     auto results_ptr = make_unique<vector<Matrix_3d>>(matrices);
     for_each(results_ptr->begin(), results_ptr->end(), 
         [](Matrix_3d& this_mat) {
@@ -106,7 +106,7 @@ unique_ptr<vector<Matrix_3d>> filpXHeading (const vector<Matrix_3d>& matrices) {
     return results_ptr;
 }
 
-unique_ptr<vector<Matrix_3d>> filpYHeading (const vector<Matrix_3d>& matrices) {
+unique_ptr<vector<Matrix_3d>> flipYHeading (const vector<Matrix_3d>& matrices) {
     auto results_ptr = make_unique<vector<Matrix_3d>>(matrices);
     for_each(results_ptr->begin(), results_ptr->end(), 
         [](Matrix_3d& this_mat) {
@@ -122,7 +122,7 @@ unique_ptr<vector<Matrix_3d>> filpYHeading (const vector<Matrix_3d>& matrices) {
     return results_ptr;
 }
 
-unique_ptr<vector<Matrix_3d>> filpZHeading (const vector<Matrix_3d>& matrices) {
+unique_ptr<vector<Matrix_3d>> flipZHeading (const vector<Matrix_3d>& matrices) {
     auto results_ptr = make_unique<vector<Matrix_3d>>(matrices);
     for_each(results_ptr->begin(), results_ptr->end(), 
         [](Matrix_3d& this_mat) {
@@ -152,15 +152,68 @@ unique_ptr<vector<Matrix_3d>> invert (const vector<Matrix_3d>& matrices) {
     return results_ptr;
 }
 
-unique_ptr<unordered_set<Matrix_3d>> getUniqueOrientationMatrices() {
-    auto results_ptr = make_unique<unordered_set<Matrix_3d>>();
+unique_ptr<set<Matrix_3d>> getUniqueOrientationMatrices() { // only 19, gotta do more variety or make the table yourself
+    auto results_ptr = make_unique<set<Matrix_3d>>(); // invert inverts all 3, x/y/z inverts just 1, try inverting all 1s and 2s in addition
     // original rotation matrices
+    for_each(ROTATION_MATRICES_AROUND_X.begin(), 
+             ROTATION_MATRICES_AROUND_X.end(), 
+             [&](const Matrix_3d& mat) {
+                results_ptr->insert(mat);
+            });
+    for_each(ROTATION_MATRICES_AROUND_Y.begin(), 
+             ROTATION_MATRICES_AROUND_Y.end(), 
+             [&](const Matrix_3d& mat) {
+                results_ptr->insert(mat);
+            });
+    for_each(ROTATION_MATRICES_AROUND_Z.begin(), 
+             ROTATION_MATRICES_AROUND_Z.end(), 
+             [&](const Matrix_3d& mat) {
+                results_ptr->insert(mat);
+            });
 
     // flipped rotation matrices
+    auto flipped_x = *flipXHeading(ROTATION_MATRICES_AROUND_X);
+    for_each(flipped_x.begin(), flipped_x.end(), [&](Matrix_3d& mat) {
+        results_ptr->insert(mat);
+    });
+    auto flipped_y = *flipYHeading(ROTATION_MATRICES_AROUND_Y);
+    for_each(flipped_y.begin(), flipped_y.end(), [&](Matrix_3d& mat) {
+        results_ptr->insert(mat);
+    });
+    auto flipped_z = *flipZHeading(ROTATION_MATRICES_AROUND_Z);
+    for_each(flipped_z.begin(), flipped_z.end(), [&](Matrix_3d& mat) {
+        results_ptr->insert(mat);
+    });
 
     // inverted rotation matrices
+    auto inverted_x = *invert(ROTATION_MATRICES_AROUND_X);
+    for_each(inverted_x.begin(), inverted_x.end(), [&](Matrix_3d& mat) {
+        results_ptr->insert(mat);
+    });
+    auto inverted_y = *invert(ROTATION_MATRICES_AROUND_Y);
+    for_each(inverted_y.begin(), inverted_y.end(), [&](Matrix_3d& mat) {
+        results_ptr->insert(mat);
+    });
+    auto inverted_z = *invert(ROTATION_MATRICES_AROUND_Z);
+    for_each(inverted_z.begin(), inverted_z.end(), [&](Matrix_3d& mat) {
+        results_ptr->insert(mat);
+    });
 
     // inverted & flipped rotation matrices
+    auto inverted_flipped_x = *invert(vector<Matrix_3d>(forward<const vector<Matrix_3d>&>(flipped_x)));
+    for_each(inverted_flipped_x.begin(), inverted_flipped_x.end(), [&](Matrix_3d& mat) {
+        results_ptr->insert(mat);
+    });
+    auto inverted_flipped_y = *invert(vector<Matrix_3d>(forward<const vector<Matrix_3d>&>(flipped_y)));
+    for_each(inverted_flipped_y.begin(), inverted_flipped_y.end(), [&](Matrix_3d& mat) {
+        results_ptr->insert(mat);
+    });    
+    auto inverted_flipped_z = *invert(vector<Matrix_3d>(forward<const vector<Matrix_3d>&>(flipped_z)));
+    for_each(inverted_flipped_z.begin(), inverted_flipped_z.end(), [&](Matrix_3d& mat) {
+        results_ptr->insert(mat);
+    });
+
+    return results_ptr;
 }
 
 void test() {
@@ -181,7 +234,7 @@ void test() {
     assert(result == (Vector_3d{-1, 3, -2}));
 
     // flip axis
-    auto flipped = *filpXHeading(ROTATION_MATRICES_AROUND_X);
+    auto flipped = *flipXHeading(ROTATION_MATRICES_AROUND_X);
     assert(flipped[0] 
             == (Matrix_3d{
                 Vector_3d{-1, 0, 0}, 
@@ -199,6 +252,10 @@ void test() {
                 Vector_3d{0, 0, -1}
             })
     ); // TODO: expand test for all entries.
+
+    // get all orientation transformation matrices
+    auto all_transformations = *getUniqueOrientationMatrices();
+    assert(all_transformations.size() == 24);
 }
 
 int main() {
